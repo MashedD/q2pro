f2a61e5857c4cf8a2b7d1502de25cfe4e0d7bbbb
MashedD <sl8code@gmail.com>
2026-03-31 09:15:41 +0200

    Compile curl statically for Windows builds

--- build-win64.sh
+++ build-win64.sh
@@ -6,7 +6,7 @@ meson setup build-win64 \
     --buildtype=release \
     -Ddefault_library=static \
     -Dc_link_args="-static -static-libgcc" \
-    --force-fallback-for=libcurl,zlib,libjpeg,libpng \
+    --force-fallback-for=zlib,libjpeg,libpng \
     -Danticheat-server=true || :
 meson compile -C build-win64 -j$(nproc)
 chmod -x build-win64/gamex86_64.dll
