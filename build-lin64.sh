2179480e1c08cd537956d441b57855ddda742bc6
MashedD <sl8code@gmail.com>
2026-06-16 20:54:00 +0200

    Disable system wide install for Linux

--- build-lin64.sh
+++ build-lin64.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-meson setup build-lin64 --buildtype=release -Danticheat-server=true || :
+meson setup build-lin64 --buildtype=release -Dsystem-wide=false -Danticheat-server=true || :
 meson compile -C build-lin64
 sstrip build-lin64/gamex86_64.so
 sstrip build-lin64/q2pro
