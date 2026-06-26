MashedD/q2pro

commit 36164007225c1d914b38475074b8cd5dd04384ee

Reduce number of GL calls to bind texture.

src/refresh/state.c
@@ -47,9 +47,12 @@ void GL_BindTexture(GLuint tmu, GLuint texnum)
     if (gls.texnums[tmu] == texnum)
         return;
 
-    GL_ActiveTexture(tmu);
-
-    qglBindTexture(GL_TEXTURE_2D, texnum);
+    if (qglBindTextureUnit) {
+        qglBindTextureUnit(tmu, texnum);
+    } else {
+        GL_ActiveTexture(tmu);
+        qglBindTexture(GL_TEXTURE_2D, texnum);
+    }
     gls.texnums[tmu] = texnum;
 
     c.texSwitches++;

src/refresh/tess.c
@@ -455,10 +455,10 @@ void GL_Flush3D(void)
 
         if (q_unlikely(gl_lightmap->integer))
             state &= ~GLS_INTENSITY_ENABLE;
-    }
 
-    if (tess.texnum[2])
-        state |= GLS_GLOWMAP_ENABLE;
+        if (tess.texnum[2])
+            state |= GLS_GLOWMAP_ENABLE;
+    }
 
     if (!(state & GLS_TEXTURE_REPLACE))
         array |= GLA_COLOR;
@@ -466,9 +466,20 @@ void GL_Flush3D(void)
     GL_StateBits(state);
     GL_ArrayBits(array);
 
-    for (int i = 0; i < MAX_TMUS; i++)
-        if (tess.texnum[i])
+    if (qglBindTextures) {
+        int count = 0;
+        for (int i = 0; i < MAX_TMUS && tess.texnum[i]; i++) {
+            if (gls.texnums[i] != tess.texnum[i]) {
+                gls.texnums[i] = tess.texnum[i];
+                count = i + 1;
+            }
+        }
+        if (count)
+            qglBindTextures(0, count, tess.texnum);
+    } else {
+        for (int i = 0; i < MAX_TMUS && tess.texnum[i]; i++)
             GL_BindTexture(i, tess.texnum[i]);
+    }
 
     if (gl_static.world.vertices)
         GL_LockArrays(tess.numverts);

src/refresh/qgl.c
@@ -314,6 +314,24 @@ static const glsection_t sections[] = {
         }
     },
 
+    // GL 4.4
+    {
+        .ver_gl = QGL_VER(4, 4),
+        .functions = (const glfunction_t []) {
+            QGL_FN(BindTextures),
+            { NULL }
+        }
+    },
+
+    // GL 4.5
+    {
+        .ver_gl = QGL_VER(4, 5),
+        .functions = (const glfunction_t []) {
+            QGL_FN(BindTextureUnit),
+            { NULL }
+        }
+    },
+
     // GL 4.5
     // GL_ARB_robustness
     {

src/refresh/qgl.h
@@ -166,7 +166,11 @@ QGLAPI void (APIENTRYP qglDepthRangef)(GLfloat n, GLfloat f);
 // GL 4.3
 QGLAPI void (APIENTRYP qglDebugMessageCallback)(GLDEBUGPROC callback, const void *userParam);
 
+// GL 4.4
+QGLAPI void (APIENTRYP qglBindTextures)(GLuint first, GLsizei count, const GLuint *textures);
+
 // GL 4.5
+QGLAPI void (APIENTRYP qglBindTextureUnit)(GLuint unit, GLuint texture);
 QGLAPI void (APIENTRYP qglReadnPixels)(GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, GLsizei bufSize, void *data);
 
 // GL_ARB_fragment_program

commit db573b9afc377d3accb5c90a786b6a6f5f1ac875

fix(refresh): avoid direct texture bind paths
Some OpenGL drivers render broken textures when using direct
or multi-bind texture APIs. Use active texture binding path
for draw state while keeping texture cache behavior intact.
Problem was introduced in 36164007225c1d914b38475074b8cd5dd04384ee

src/refresh/state.c
@@ -49,12 +49,8 @@ void GL_BindTexture(glTmu_t tmu, GLuint texnum)
     if (gls.texnums[tmu] == texnum)
         return;
 
-    if (qglBindTextureUnit) {
-        qglBindTextureUnit(tmu, texnum);
-    } else {
-        GL_ActiveTexture(tmu);
-        qglBindTexture(GL_TEXTURE_2D, texnum);
-    }
+    GL_ActiveTexture(tmu);
+    qglBindTexture(GL_TEXTURE_2D, texnum);
     gls.texnums[tmu] = texnum;
 
     c.texSwitches++;
@@ -81,12 +77,8 @@ void GL_BindCubemap(GLuint texnum)
     if (gls.texnumcube == texnum)
         return;
 
-    if (qglBindTextureUnit) {
-        qglBindTextureUnit(TMU_TEXTURE, texnum);
-    } else {
-        GL_ActiveTexture(TMU_TEXTURE);
-        qglBindTexture(GL_TEXTURE_CUBE_MAP, texnum);
-    }
+    GL_ActiveTexture(TMU_TEXTURE);
+    qglBindTexture(GL_TEXTURE_CUBE_MAP, texnum);
     gls.texnumcube = texnum;
 
     c.texSwitches++;

src/refresh/tess.c
@@ -670,24 +670,12 @@ void GL_Flush3D(void)
 
     if (state & GLS_DEFAULT_SKY) {
         GL_BindCubemap(tess.texnum[TMU_TEXTURE]);
-    } else if (qglBindTextures) {
-#if USE_DEBUG
-        if (q_unlikely(gl_nobind->integer))
-            tess.texnum[TMU_TEXTURE] = TEXNUM_DEFAULT;
-#endif
-        int count = 0;
-        for (int i = 0; i < MAX_TMUS && tess.texnum[i]; i++) {
-            if (gls.texnums[i] != tess.texnum[i]) {
-                gls.texnums[i] = tess.texnum[i];
-                count = i + 1;
-                c.texSwitches++;
-            }
-        }
-        if (count)
-            qglBindTextures(0, count, tess.texnum);
     } else {
-        for (int i = 0; i < MAX_TMUS && tess.texnum[i]; i++)
+        for (int i = 0; i < MAX_TMUS; i++) {
+            if (!tess.texnum[i])
+                continue;
             GL_BindTexture(i, tess.texnum[i]);
+        }
     }
 
     GL_DrawIndexed(SHOWTRIS_WORLD);